6233084fc801a1af5e098c691dd86cfc502843c8
RVS-502 <rodelval8@gmail.com>
2024-01-23 23:03:08 -0600

    initcommit

---
+++ cordelia/index_cordelia.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="cordelia-icon.png" type="imagen/x-icon">
+    <title>Cordelia's Knitting</title>
+</head>
+<body>
+    <header>
+    <h1>CORDELIA'S KNITTING</h1>
+    <h2>Cordelia's Knitting es un emprendimiento guatemalteco de tejido en <strong>Tricot</strong> y <strong>Crochet</strong></h2>
+    <nav>
+        <a href="" class="nav-link">Inicio</a>
+        <a href="" class="nav-link">Tricot</a>
+        <a href="" class="nav-link">Crochet</a>
+        <a href="" class="nav-link">Proyectos</a>
+    </nav>
+    </header>
+
+    <div class="sidebar">
+        <div class="logo-details">
+            <i class="bx bxl-c-plus-plus icon"></i>
+            <div class="logo_name">Cordelia</div>
+            <i class="bx bx-menu" id="btn"></i>
+        </div>
+        
+        <ul class="nav-list">
+            <li>
+                <i class="bx bx-search"></i>
+                <input type="text" placeholder="Search...">
+                <span class="tooltip">Search</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bx bx-grid-alt"></i>
+                    <span class="links_name">Dashboard</span>
+                </a>
+                <span class="tooltip">Dashboard</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bx bx-user"></i>
+                    <span class="links_name">User</span>
+                </a>
+                <span class="tooltip">User</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bx bx-chat"></i>
+                    <span class="links_name">Messages</span>
+                </a>
+                <span class="tooltip">Messages</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bx bx-pie-chart-alt-2"></i>
+                    <span class="links_name">Analytics</span>
+                </a>
+                <span class="tooltip">Analytics</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bx bx-folder"></i>
+                    <span class="links_name">File Manager</span>
+                </a>
+                <span class="tooltip">Files</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bx bx-cart-alt></i>
+                    <span class="links_name">Order</span>
+                </a>
+                <span class="tooltip">Order</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bx bx-heart"></i>
+                    <span class="links_name">Saved</span>
+                </a>
+                <span class="tooltip">Saved</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bx bx-cog"></i>
+                    <span class="links_name">Settings</span>
+                </a>
+                <span class="tooltip">Settings</span>
+            </li>
+            <li class="porfile">
+                <div class="profile-details">
+                    <img src="Your image" alt="profileImage"?
+                    <div class="name_job">
+                        <div class="name" >Cordelia Army</div>
+                        <div class="job">Web designer</div>
+                    </div>
+                </div>
+                <i class="bx bx-log-out" id="log_out"></i>
+            </li>
+        </ul>
+    </div>
+    <section class="home-section">
+        <div class="text">Dashboard</div>
+    </section>
+</body>
+</html>
